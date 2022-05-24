b5c58f0073aea5bac765ea263ad7ca0aca66e795
Baran Mordoğan <77172909+okunamayanad@users.noreply.github.com>
2022-05-24 23:50:08 +0300

    Add Welcome message and perm check

--- windows-10-pro.bat
+++ windows-10-pro.bat
@@ -1,4 +1,40 @@
 :: Admin Rights Checker
+@echo off
+
+echo Windows 10 Professional Cracker
+echo made by these cool people:
+FOR /F %%i IN (contributors.txt) DO @echo %%i
+echo .
+echo To continue press any button.
+pause
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
 
 :: Key Applier
 :: KMS Changer
